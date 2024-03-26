9a84ce16aa801dc922d51485583d3ad0b59317e5
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-03-26 10:22:30 +0900

    ReactQueryDevtools 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from '../src/css/GlobalStyle';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient()
 
@@ -14,6 +15,7 @@ root.render(
     {/* <GlobalStyle/> */}
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
     </QueryClientProvider>
   </>
 );
